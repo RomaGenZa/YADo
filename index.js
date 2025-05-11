2eaa4f1c841ed93a8d491f0635abdd4f46af4df6
Roman <romagilda@yandex.ru>
2025-05-11 22:58:15 +0300

    add schedule poligons

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const infoString = document.querySelector('.btn_memo');
 const infoConteiner = document.querySelector('#mem');
+const infoSchedule = document.querySelector('.button_content_info');
+const schedulePoligon = document.querySelector('#poligon');
 
 infoString.addEventListener('click', () => {
   infoConteiner.classList.toggle('memo');
   infoConteiner.classList.toggle('hidden');
+})
+
+infoSchedule.addEventListener('click', () => {
+  schedulePoligon.classList.toggle('schedule');
+  schedulePoligon.classList.toggle('hidden');
 })
