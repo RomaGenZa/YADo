1f104360044f82cfe9ec2934fde1976e05338199
Roman <romagilda@yandex.ru>
2025-05-17 17:05:55 +0300

    add button availability

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const infoString = document.querySelector('.btn_memo');
 const infoConteiner = document.querySelector('#mem');
 const infoSchedule = document.querySelector('.button_content_info');
 const schedulePoligon = document.querySelector('#poligon');
+const btnAvailability = document.querySelector('#btnAvailability');
+const infoAvailability = document.querySelector('#availability');
 
 infoString.addEventListener('click', () => {
   infoConteiner.classList.toggle('memo');
@@ -11,4 +13,9 @@ infoString.addEventListener('click', () => {
 infoSchedule.addEventListener('click', () => {
   schedulePoligon.classList.toggle('schedule');
   schedulePoligon.classList.toggle('hidden');
+})
+
+btnAvailability.addEventListener('click', () => {
+  infoAvailability.classList.toggle('schedule');
+  infoAvailability.classList.toggle('hidden');
 })
